d51660e7674a765ed0b88f936b086c1bbd242355
Celdama <celdama.dev@gmail.com>
2022-01-06 15:11:26 +0100

    src: create and style interest component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Info from './Components/Info';
 import About from './Components/About';
+import Interests from './Components/Interests';
 
 function App() {
   return (
     <div className='App'>
       <Info />
       <About title='About' />
+      <Interests title='Interests' />
     </div>
   );
 }
