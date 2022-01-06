fd0f5f787f05999b30f546ccb455a589db827f2f
Celdama <celdama.dev@gmail.com>
2022-01-06 15:09:08 +0100

    src: create and style About component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Info from './Components/Info';
+import About from './Components/About';
 
 function App() {
   return (
     <div className='App'>
       <Info />
+      <About title='About' />
     </div>
   );
 }
